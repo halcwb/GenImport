66e7a52759258c49bf9c4eeb65f8c24cae6ba976
rdvanbuuren <rdvanbuuren@gmail.com>
2012-06-12 11:49:10 +0200

    first commit

---
+++ Informedica.GenImport.Acceptance/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Informedica.GenImport.Acceptance
+{
+    public class HelloWorld
+    {
+        public string SayHelloWorld ()
+        {
+            return "Hello World";
+        }
+    }
+}
