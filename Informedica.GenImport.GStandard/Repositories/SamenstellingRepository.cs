e29624ad21c783368a0d4bb81cf88d280f8315d7
rdvanbuuren <rdvanbuuren@gmail.com>
2012-07-03 11:58:45 +0200

    Added SamenstellingImportService unit tests.

---
+++ Informedica.GenImport.GStandard/Repositories/SamenstellingRepository.cs
@@ -0,0 +1,13 @@
+﻿using Informedica.DataAccess.Repositories;
+using Informedica.GenImport.GStandard.DomainModel;
+using NHibernate;
+
+namespace Informedica.GenImport.GStandard.Repositories
+{
+    public class SamenstellingRepository : NHibernateRepository<Samenstelling, int>
+    {
+        public SamenstellingRepository(ISessionFactory factory) : base(factory)
+        {
+        }
+    }
+}
---
+++ Informedica.GenImport.GStandard/Services/SamenstellingenImportService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Informedica.GenImport.DataAccess;
+using Informedica.GenImport.GStandard.DomainModel.Interfaces;
+using NHibernate;
+
+namespace Informedica.GenImport.GStandard.Services
+{
+    public class SamenstellingenImportService : GStandardImportServiceBase<ISamenstelling>
+    {
+        public SamenstellingenImportService(string databaseFilePath, IFileSerializerBase<ISamenstelling> fileSerializer, ISessionFactory sessionFactory) : base(databaseFilePath, fileSerializer, sessionFactory)
+        {
+        }
+
+        public override void Import(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
