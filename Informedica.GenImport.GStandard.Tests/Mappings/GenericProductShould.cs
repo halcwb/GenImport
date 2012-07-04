cfcd8f88640af5a85225eea18d1b76b6239120ae
rdvanbuuren <rdvanbuuren@gmail.com>
2012-07-04 13:34:23 +0200

    Added Unit tests for GenericProduct.

---
+++ Informedica.GenImport.GStandard.Tests/Mappings/GenericProductShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using FluentNHibernate.Testing;
+using Informedica.GenImport.GStandard.DomainModel;
+using Informedica.GenImport.GStandard.DomainModel.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Informedica.GenImport.GStandard.Tests.Mappings
+{
+    [TestClass]
+    public class GenericProductShould : TestSessionContext
+    {
+        [TestMethod]
+        public void Correctly_Map_GenericProduct()
+        {
+            new PersistenceSpecification<GenericProduct>(CurrentSession)
+                .CheckProperty(b => b.MutKod, MutKod.RecordNotChanged)
+                .CheckProperty(b => b.GpInSt, "A")
+                .CheckProperty(b => b.GpKode, 1)
+                .CheckProperty(b => b.GpKtVr, (short)1)
+                .CheckProperty(b => b.GpKTwg, (short)1)
+                .CheckProperty(b => b.GpNmNr, 1)
+                .CheckProperty(b => b.GpStNr, 1)
+                .CheckProperty(b => b.GsKode, 1)
+                .CheckProperty(b => b.SpKode, 1)
+                .CheckProperty(b => b.ThEhHv, (short)1)
+                .CheckProperty(b => b.ThKtVr, (short)1)
+                .CheckProperty(b => b.XpEhHv, (short)1)
+                .VerifyTheMappings();
+        }
+    }
+}
---
+++ Informedica.GenImport.GStandard.Tests/Services/GenericProductImportServiceShould.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Informedica.GenImport.DataAccess;
+using Informedica.GenImport.GStandard.DomainModel;
+using Informedica.GenImport.GStandard.DomainModel.Enums;
+using Informedica.GenImport.GStandard.DomainModel.Interfaces;
+using Informedica.GenImport.GStandard.Repositories;
+using Informedica.GenImport.GStandard.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NHibernate;
+
+namespace Informedica.GenImport.GStandard.Tests.Services
+{
+    [TestClass]
+    public class GenericProductImportServiceShould : TestSessionContext
+    {
+        #region Helpers
+
+        private class ImportServiceMock : GenericProductImportService
+        {
+            public ImportServiceMock(string databasePath, IFileSerializerBase<IGenericProduct> fileSerializer, ISessionFactory sessionFactory)
+                : base(databasePath, fileSerializer, sessionFactory)
+            {
+            }
+
+            public new void Import(Stream stream)
+            {
+                base.Import(stream);
+            }
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void Import_The_GenericProduct_From_A_Stream_And_Create_An_Entity_In_The_Database()
+        {
+            const int expectedCount = 1;
+            var lines = new List<IGenericProduct>{
+                                                     new GenericProduct{
+                                                                           GpInSt = "A",
+                                                                           GpKode = 1,
+                                                                           GpKtVr = 1,
+                                                                           GpKTwg = 1,
+                                                                           GpNmNr = 1,
+                                                                           GpStNr = 1,
+                                                                           GsKode = 1,
+                                                                           MutKod = MutKod.RecordNotChanged,
+                                                                           SpKode = 1,
+                                                                           ThEhHv = 1,
+                                                                           ThKtVr = 1,
+                                                                           ThKTwg = 1,
+                                                                           XpEhHv = 1
+                                                                       }
+                                                 };
+
+            var fileSerializerMock = new Mock<IFileSerializerBase<IGenericProduct>>(MockBehavior.Strict);
+            fileSerializerMock.Setup(s => s.ReadLines(It.IsAny<Stream>())).Returns(lines);
+
+            var sessionFactory = GetSessionFactory();
+
+            new ImportServiceMock("", fileSerializerMock.Object, sessionFactory).Import(new MemoryStream());
+
+            Assert.AreEqual(expectedCount, new GenericNameRepository(sessionFactory).Count);
+        }
+    }
+}
