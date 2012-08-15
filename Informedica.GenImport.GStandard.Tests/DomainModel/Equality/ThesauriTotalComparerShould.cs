3ca99859310f6a63a34b9487f397a94e00068d3d
rdvanbuuren <rdvanbuuren@gmail.com>
2012-08-15 13:03:41 +0200

    Added better implementation to the ThesauriTotalComparer.

--- Informedica.GenImport.GStandard.Tests/DomainModel/Equality/ThesauriTotalComparerShould.cs
+++ Informedica.GenImport.GStandard.Tests/DomainModel/Equality/ThesauriTotalComparerShould.cs
@@ -1,5 +1,7 @@
 ﻿using Informedica.GenImport.GStandard.DomainModel;
+using Informedica.GenImport.GStandard.DomainModel.Enums;
 using Informedica.GenImport.GStandard.DomainModel.Equality;
+using Informedica.GenImport.GStandard.DomainModel.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Informedica.GenImport.GStandard.Tests.DomainModel.Equality
@@ -7,11 +9,32 @@ namespace Informedica.GenImport.GStandard.Tests.DomainModel.Equality
     [TestClass]
     public class ThesauriTotalComparerShould
     {
-        [TestMethod]
-        public void Equal_RelationBetweenNames_When_TsNr_TsItNr_Are_Equal()
+        private static IThesauriTotal CreateThesauriTotal()
         {
-            var x = new ThesauriTotal { TsNr = 1, TsItNr = 2 };
-            var y = new ThesauriTotal { TsNr = 1, TsItNr = 2 };
+            return new ThesauriTotal
+            {
+                MutKod = MutKod.RecordNotChanged,
+                ThAKd1 = "A",
+                ThAKd2 = "B",
+                ThAKd3 = "C",
+                ThAKd4 = "D",
+                ThAKd5 = "E",
+                ThAKd6 = "F",
+                ThItMk = "G",
+                ThNm15 = "H",
+                ThNm25 = "I",
+                ThNm4 = "J",
+                ThNm50 = "K",
+                TsItNr = 1,
+                TsNr = 2
+            };
+        }
+
+        [TestMethod]
+        public void Equal_When_All_Fields_Are_Equal()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
 
             var comparer = new ThesauriTotalComparer();
             bool result = comparer.Equals(x, y);
@@ -20,10 +43,11 @@ namespace Informedica.GenImport.GStandard.Tests.DomainModel.Equality
         }
 
         [TestMethod]
-        public void Not_Equal_RelationBetweenNames_When_TsNr_Are_Different()
+        public void Not_Equal_When_MutKod_Is_Different()
         {
-            var x = new ThesauriTotal { TsNr = 1, TsItNr = 2 };
-            var y = new ThesauriTotal { TsNr = 2, TsItNr = 2 };
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.MutKod = MutKod.RecordUpdated;
 
             var comparer = new ThesauriTotalComparer();
             bool result = comparer.Equals(x, y);
@@ -32,10 +56,11 @@ namespace Informedica.GenImport.GStandard.Tests.DomainModel.Equality
         }
 
         [TestMethod]
-        public void Not_Equal_RelationBetweenNames_When_TsItNr_Are_Different()
+        public void Not_Equal_When_ThAKd1_Is_Different()
         {
-            var x = new ThesauriTotal { TsNr = 1, TsItNr = 2 };
-            var y = new ThesauriTotal { TsNr = 2, TsItNr = 2 };
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd1 = "B";
 
             var comparer = new ThesauriTotalComparer();
             bool result = comparer.Equals(x, y);
@@ -44,10 +69,172 @@ namespace Informedica.GenImport.GStandard.Tests.DomainModel.Equality
         }
 
         [TestMethod]
-        public void Return_Correct_HashCode_From_TsNr_TsItNr()
+        public void Not_Equal_When_ThAKd2_Is_Different()
         {
-            var thesauriTotal = new ThesauriTotal { TsNr = 1, TsItNr = 2 };
-            int expectedHashCode = thesauriTotal.TsNr.GetHashCode() + thesauriTotal.TsItNr.GetHashCode();
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd2 = "C";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThAKd3_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd3 = "D";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThAKd4_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd4 = "E";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThAKd5_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd5 = "F";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThAKd6_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThAKd6 = "G";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThItMk_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThItMk = "H";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThNm15_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThNm15 = "I";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThNm25_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThNm25 = "J";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThNm4_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThNm4 = "K";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_ThNm50_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.ThNm50 = "L";
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_TsItNr_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.TsItNr = 2;
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Not_Equal_When_TsNr_Is_Different()
+        {
+            var x = CreateThesauriTotal();
+            var y = CreateThesauriTotal();
+            y.TsNr = 3;
+
+            var comparer = new ThesauriTotalComparer();
+            bool result = comparer.Equals(x, y);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Return_Correct_HashCode_From_Fields()
+        {
+            var thesauriTotal = CreateThesauriTotal();
+            int expectedHashCode =
+                (byte)thesauriTotal.MutKod ^ thesauriTotal.ThAKd1.GetHashCode() ^ thesauriTotal.ThAKd2.GetHashCode() ^
+                thesauriTotal.ThAKd3.GetHashCode() ^ thesauriTotal.ThAKd4.GetHashCode() ^
+                thesauriTotal.ThAKd5.GetHashCode() ^ thesauriTotal.ThAKd6.GetHashCode() ^
+                thesauriTotal.ThItMk.GetHashCode() ^ thesauriTotal.ThNm15.GetHashCode() ^
+                thesauriTotal.ThNm25.GetHashCode() ^ thesauriTotal.ThNm4.GetHashCode() ^
+                thesauriTotal.ThNm50.GetHashCode() ^ thesauriTotal.TsNr ^ thesauriTotal.TsItNr;
 
             var comparer = new ThesauriTotalComparer();
             int result = comparer.GetHashCode(thesauriTotal);
--- Informedica.GenImport.GStandard/DomainModel/Equality/ThesauriTotalComparer.cs
+++ Informedica.GenImport.GStandard/DomainModel/Equality/ThesauriTotalComparer.cs
@@ -9,13 +9,30 @@ namespace Informedica.GenImport.GStandard.DomainModel.Equality
 
         public bool Equals(IThesauriTotal x, IThesauriTotal y)
         {
-            return x.TsNr == y.TsNr &&
+            return x.MutKod == y.MutKod &&
+                   x.ThAKd1 == y.ThAKd1 &&
+                   x.ThAKd2 == y.ThAKd2 &&
+                   x.ThAKd3 == y.ThAKd3 &&
+                   x.ThAKd4 == y.ThAKd4 &&
+                   x.ThAKd5 == y.ThAKd5 &&
+                   x.ThAKd6 == y.ThAKd6 &&
+                   x.ThItMk == y.ThItMk &&
+                   x.ThNm15 == y.ThNm15 &&
+                   x.ThNm25 == y.ThNm25 &&
+                   x.ThNm4 == y.ThNm4 &&
+                   x.ThNm50 == y.ThNm50 &&
+                   x.TsNr == y.TsNr &&
                    x.TsItNr == y.TsItNr;
         }
 
         public int GetHashCode(IThesauriTotal obj)
         {
-            return obj.TsNr.GetHashCode() + obj.TsItNr.GetHashCode();
+            return (byte)obj.MutKod ^ obj.ThAKd1.GetHashCode() ^ obj.ThAKd2.GetHashCode() ^
+                   obj.ThAKd3.GetHashCode() ^ obj.ThAKd4.GetHashCode() ^
+                   obj.ThAKd5.GetHashCode() ^ obj.ThAKd6.GetHashCode() ^
+                   obj.ThItMk.GetHashCode() ^ obj.ThNm15.GetHashCode() ^
+                   obj.ThNm25.GetHashCode() ^ obj.ThNm4.GetHashCode() ^
+                   obj.ThNm50.GetHashCode() ^ obj.TsNr ^ obj.TsItNr;
         }
 
         #endregion
