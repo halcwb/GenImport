9334efed029a26923dce953039dc66815e61b57e
Casper Bollen <halcwb@gmail.com>
2013-05-04 17:42:39 +0200

    Added generic tree builder

---
+++ Informedica.GenImport.GStandard.FSharp/TreeStructures.fs
@@ -0,0 +1,13 @@
+﻿namespace TreeStructures
+
+module TreeStructures =
+
+    let buildGenericTree isParent partf addNodes nodes =
+        let rec build nodes =
+            match nodes |> partf with
+            | roots, nodes when nodes |> List.length = 0 -> printfn "No more nodes"; roots
+            | roots, nodes  -> roots |> List.map(fun g -> g |> addNodes ((nodes |> List.filter(fun n -> g |> isParent n) |> build)))
+
+        build nodes
+     
+
